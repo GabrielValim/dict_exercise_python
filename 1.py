3ea4e8840349d09abe6154dce2ea017c22f4f6f0
GabrielValim <barros.gabriel.valim@hotmail.com>
2024-06-05 21:55:07 -0300

    exercicios dicionario

---
+++ 1.py
@@ -0,0 +1,3 @@
+# 1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+
+dict = {'name': "Valim", 'age': "22", 'city': "Rj"}
---
+++ 2.py
@@ -0,0 +1,11 @@
+# 2 - Utilizando o dicionário criado no item 1:
+
+# Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+# Adicione um campo de profissão para essa pessoa;
+# Remova um item do dicionário.
+
+dict = {'name': "Valim", 'age': 22, 'city': "Rj"}
+
+dict['age'] = 25
+dict['job'] = "Dev"
+del dict['city']
